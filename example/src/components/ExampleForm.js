84ced831a3e1d4fd0a0e00771ef5a32bc5096d39
Gonzalo Rubino <grubino@WSLMWEB05001.local>
2016-09-16 12:15:01 -0300

    Version 1.0.13 - Fixes validation

--- example/src/components/ExampleForm.js
+++ example/src/components/ExampleForm.js
@@ -1,8 +1,8 @@
 /*
 
 @author Gonzalo Rubino on Thursday, September 8th, 2016, 10:54:00 AM
-@version 1.0.8
-@Description TN y La Gente Publish Form
+@version 1.0.13
+@Description Example Publish Form
 
 */
 
@@ -26,49 +26,49 @@ const ContentInside = React.createClass({
               rows={3}
               cols={40}
               name="title"
-              label="¿Que Pas&oacute;?"
-              placeholder="Ejemplo: Un colectivo chocó contra un auto."
+              label="What happens?"
+              placeholder="Ex: A car was hitten by a bus."
               validations="minLength:5"
               required
               validationErrors={{
-                    minLength: 'Este campo es requerido. Ingrese al menos 5 caracteres.'
+                    minLength: 'This field is required. Insert at least 5 chars.'
                 }}
             />
             <Textarea
               rows={3}
               cols={40}
               name="location"
-              label="¿D&oacute;nde y cu&aacute;ndo fue?"
-              placeholder="Ejemplo: Ocurrió en Avellaneda, el sábado a las 10 de la mañana."
+              label="Where was it?"
+              placeholder="Ex: Near the Avenue, at the end of the street."
               validations="minLength:5"
               required
               validationErrors={{
-                    minLength: 'Este campo es requerido. Ingrese al menos 5 caracteres.'
+                    minLength: 'This field is required. Insert at least 5 chars.'
                 }}
             />
             <Textarea
               rows={3}
               cols={40}
               name="body"
-              label="Contanos m&aacute;s detalles o describ&iacute; tus fotos o videos"
-              placeholder="Ejemplo: Un auto blanco circulaba a gran velocidad por la Avenida Mitre. Cruzó en rojo y chocó contra un colectivo."
+              label="Tell us more details please."
+              placeholder="Ex: Fill it with your inner reporter!"
               validations="minLength:5"
               required
               validationErrors={{
-                    minLength: 'Este campo es requerido. Ingrese al menos 5 caracteres.'
+                    minLength: 'This field is required. Insert at least 5 chars.'
                 }}
             />
         <Input
           name="phone"
           type="tel"
           value=""
-          label="¿A qu&eacute; n&uacute;mero de tel&eacute;fono podemos contactarte?"
-          placeholder="Ejemplo: 54 (911) 4444 4444"
+          label="Can you give us a number to get in contact for more info?"
+          placeholder="Ejemplo: 54 911 4444 4444"
           validations="isNumeric,minLength:8"
           required
           validationErrors={{
-                    minLength: 'Por favor ingrese un numero telefonico.',
-                    isNumeric: 'Por favor ingrese solo numeros.'
+                    minLength: 'Please enter a phone number.',
+                    isNumeric: 'Please numbers only.'
                 }}
         />
       </fieldset>
@@ -92,8 +92,7 @@ export default class ExampleForm extends React.Component {
     return (
       <EmptyForm
         insideForm={<ContentInside />}
-        formClass='tnylg-publish form-vertical'
-        invalidFormAction=""
+        formClass='form-vertical'
         submitValue="Publish it!"
         submitAction={this.submit}/>
     );
